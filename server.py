a06e08e1282a63fe729b011b84ffee488415c420
Prasanna Anthony <77713617+DinoQuinten@users.noreply.github.com>
2025-02-16 22:39:44 +0530

    Update server.py

--- server.py
+++ server.py
@@ -8,22 +8,36 @@ def sum_route():
     num1 = float(request.args.get('num1'))
     num2 = float(request.args.get('num2'))
     # Write your code here
+    num1 = float(request.args.get('num1'))
+    num2 = float(request.args.get('num2'))
+    res=summation(num1,num2)
+    return str (res)
+
 
 @app.route("/sub")
 def sub_route():
     num1 = float(request.args.get('num1'))
     num2 = float(request.args.get('num2'))
     # Write your code here
+    num1 = float(request.args.get('num1'))
+    num2 = float(request.args.get('num2'))
+    res=substraction(num1,num2)
+    return str (res)
 
 @app.route("/mul")
 def mul_route():
     num1 = float(request.args.get('num1'))
     num2 = float(request.args.get('num2'))
     # Write your code here  
+    num1 = float(request.args.get('num1'))
+    num2 = float(request.args.get('num2'))
+    res=mul(num1,num2)
+    return str (res)
 
 @app.route("/")
 def render_index_page():
     # Write your code here
-    
+    return render_template('index.html')
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
